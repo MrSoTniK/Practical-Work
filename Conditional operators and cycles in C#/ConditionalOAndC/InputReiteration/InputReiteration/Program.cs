f7023d1bfd5678320fd3d286667ed9a2ef0a96b6
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-01-11 21:13:29 +0300

    Add files via upload

---
+++ Conditional operators and cycles in C#/ConditionalOAndC/InputReiteration/InputReiteration/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputReiteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            int reiterationNumber = 0, reiterationStep = 0;
+            bool isNumber = false;
+
+            Console.WriteLine("Введите сообщение");
+            userInput = Console.ReadLine();
+            Console.WriteLine("Введите количество повторений");
+            while(isNumber == false)
+            {
+                isNumber = Int32.TryParse(Console.ReadLine(), out reiterationNumber);
+                if(isNumber == false)
+                {
+                    Console.WriteLine("Введено не число. Попробуйте снова.");
+                }               
+            }
+
+            while (reiterationStep != reiterationNumber) 
+            {
+                Console.WriteLine(userInput);
+                reiterationStep++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
