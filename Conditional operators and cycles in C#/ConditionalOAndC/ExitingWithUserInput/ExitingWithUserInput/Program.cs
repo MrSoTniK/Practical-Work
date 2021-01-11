acce09a402879c1cffee90d66eeb07a6a4d19b37
MrSoTniK <48535003+MrSoTniK@users.noreply.github.com>
2021-01-12 00:04:51 +0300

    Add files via upload

---
+++ Conditional operators and cycles in C#/ConditionalOAndC/ExitingWithUserInput/ExitingWithUserInput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExitingWithUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            while (true) 
+            {
+                userInput = Console.ReadLine();
+                if (userInput == "exit") 
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
